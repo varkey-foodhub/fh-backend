8a58a5d90234c96ec6234a2b30a7432c25a6e026
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-01-12 11:03:02 +0530

    feat(menu): Implemented '/menu' into app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import type {Reqest,Response} from 'express'
 import { errorMiddleware } from './middlewares/error.middleware'
+import menuRouter from './modules/menu/menu.router'
 const app = express()
 
 app.use(express.json())
@@ -12,6 +13,8 @@ app.get('/health',(_req:Reqest,res:Response)=>{
     })
 
 })
+
+app.use('/menu',menuRouter)
 
 app.use(errorMiddleware)
 
