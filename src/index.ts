05b2960f20bcedf9550d1b68a2c1c606cff581c1
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-01-12 10:13:50 +0530

    feat(express): Setup a simple express server with '/alive' endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("TypeScript works")
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
