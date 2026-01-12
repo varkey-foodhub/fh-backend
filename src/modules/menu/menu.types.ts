ae64ac99181b5ba8c1fdba0ea7db96f93c186076
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-01-12 10:20:07 +0530

    feat(types): Setup types for menu and restaurant

---
+++ src/modules/menu/menu.types.ts
@@ -0,0 +1,10 @@
+export type MenuItem = {
+    name:string,
+    price:number,
+    ingredients:string[]
+}
+
+export type Menu = {
+    items: MenuItem[];
+}
+
---
+++ src/modules/restaurant/restaurant.type.ts
@@ -0,0 +1,6 @@
+import type { Menu } from "../menu/menu.types"
+export type Restaurant = {
+    name:string,
+    description:string,
+    menu: Menu;
+}
