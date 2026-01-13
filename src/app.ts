c322a2cc580f09786022ed3902a9f00e2b46629c
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-01-13 11:52:56 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import type {Reqest,Response} from 'express'
 import { errorMiddleware } from './middlewares/error.middleware'
 import menuRouter from './modules/menu/menu.router'
 import restaurantRouter from './modules/restaurant/restaurant.route'
+import cors from 'cors'
 const app = express()
 
 app.use(express.json())
+app.use(cors());
+  
 
 app.get('/health',(_req:Reqest,res:Response)=>{
     res.json({
